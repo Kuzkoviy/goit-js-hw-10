bba6ea014974a86145ea3b1453b87d71bd208a83
Kuzkoviy <72725653+Kuzkoviy@users.noreply.github.com>
2024-05-11 20:51:32 +0300

    upd

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -8,13 +8,40 @@ import iziToast from "izitoast";
 // Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
+const timePicker = document.querySelector('#datatime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysCounter = document.querySelector('.value[data-days]');
+const hoursCounter = document.querySelector('.value[data-hours]');
+const minutesCounter = document.querySelector('.value[data-minutes]');
+const secondsCounter = document.querySelector('.value[data-seconds]');
 
-const timePicker = document.querySelector('input[type="text]'),
-        startBtn = document.querySelector('button[data-start]');
-        daysData =  document.querySelector('.value[data-days]'),
-        hoursData = document.querySelector('.value[data-hours]'),
-        minutesData = document.querySelector('.value[data-minutes]'),
-        secondsData = document.querySelector('.value[data-seconds]');
+let selectedTime;
+
+const options = {
+        enableTime: true,
+        time_24hr: true,
+        defaultDate: new Date(),
+        minuteIncrement: 1,
+        onClose(selectedDates) {
+          console.log(selectedDates[0]);
+          
+          const dateNow = selectedDates[0].defaultDate.getTime();
+          const futureDate = selectedDates[0].getTime();
+
+          if(futureDate < dateNow) {
+                iziToast.warning({
+                        title: 'Error',
+                        message: 'Please choose a date in the future',
+                });
+                startBtn.disabled = true;
+          }     else {
+                startBtn.disabled = false;
+                selectedTime.selectedDates[0].getTime();
+          }
+        },
+      };
+      
+      flatpickr('#datatime-picker', options);
 
         
 
